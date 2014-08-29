abe68f1a8052a271a2f094f986741f9ce3116d68
Dmytro Sokil <dmytro.sokil@gmail.com>
2014-08-29 23:46:39 +0300

    implement component

---
+++ src/Sokil/Mongo/Yii/LogRoute.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Sokil\Mongo\Yii;
+
+class LogRoute extends CLogRoute 
+{
+    protected function processLogs($logs) 
+    {
+        $logCollection = \Yii::app()->mongo->getCollection('log');
+        
+        foreach ($logs as $log) {
+            
+            $logCollection
+                ->createDocument()
+                ->setLevel($log[1])
+                ->setCategory($log[2])
+                ->setTime($log[3])
+                ->setMessage($log[0])
+                ->save();
+        }
+    }
+
+}
